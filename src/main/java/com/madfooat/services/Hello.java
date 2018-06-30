85a98157770b17118aff1443cdc690e1023b8b39
Mohamed Aboulzahab <mohamed_aboulzahab@EFinance.com.eg>
2018-06-30 21:53:44 +0200

    first commit1

--- src/main/java/com/madfooat/services/Hello.java
+++ src/main/java/com/madfooat/services/Hello.java
@@ -1,5 +1,10 @@
 package com.madfooat.services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,19 @@ public class Hello {
 	
 	@RequestMapping("/")
 	public String index() {
+		 Path dirPathObj = Paths.get("/zahab");
+		   try {
+			   dirPathObj = Files.createDirectories(dirPathObj);
+			   System.out.println("dirPathObj >> "+ dirPathObj);
+			   
+			System.out.println("! New Directory Successfully Created !");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("! New Directory Error Created !");
+		}
+		   
         return "Greetings from Spring Boot! hi";
     }
 
