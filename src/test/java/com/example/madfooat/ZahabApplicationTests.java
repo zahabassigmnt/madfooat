4595e2174165c83b13f9884a32c08a410f5281d1
Mohamed Aboulzahab <mohamed_aboulzahab@EFinance.com.eg>
2018-07-04 02:00:30 +0200

    first commit

--- src/test/java/com/example/madfooat/ZahabApplicationTests.java
+++ src/test/java/com/example/madfooat/ZahabApplicationTests.java
@@ -7,8 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration
+//@SpringBootTest
 public class ZahabApplicationTests {
 
 	@Test
